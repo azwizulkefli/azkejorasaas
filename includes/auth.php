4a3515e8efbf6f9b97b5d02bae07dd6cabf35de7
azwizulkefli <azwizulkefli@gmail.com>
2026-08-17 11:57:08 +0800

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,7 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../settings.php';
+require_once __DIR__ . '/settings.php';
 
 function login(string $email, string $password): bool {
     global $pdo;
