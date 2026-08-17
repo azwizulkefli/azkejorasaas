207783d3a30ecefc412c741bce774e2b53961e5b
azwizulkefli <azwizulkefli@gmail.com>
2026-08-17 11:37:04 +0800

    Create activate.php

---
+++ public/activate.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/../includes/auth.php';
+$token = $_GET['token'] ?? '';
+if ($token === '') { header('Location: index.php?err=notoken'); exit; }
+
+$res = activateByToken($token);
+if (!$res['ok']) {
+    header('Location: index.php?err=' . urlencode($res['error'])); exit;
+}
+header('Location: /subscriber/main.php?welcome=1');
+exit;
